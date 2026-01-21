69371ffb2119da58c05179f001e4605d0787dfd6
ANCY PETER <ancypeter2k@gmail.com>
2026-01-21 20:06:46 +0530

    Product List

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  devIndicators: {
-    buildActivity: false,
-  },
 };
 
 export default nextConfig;
